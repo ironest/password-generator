7b1503657df2222e820093ac74d169495b617201
= <=>
2025-07-03 09:20:45 +1000

    Align figma design to output password component

--- src/components/output/index.tsx
+++ src/components/output/index.tsx
@@ -5,13 +5,13 @@ interface OutputProps {
 
 export const OutputPassword = ({ loading, password }: OutputProps) => {
   return (
-    <p className="flex justify-between items-center w-full bg-base-200 p-4">
+    <p className="flex justify-between items-center w-full bg-base-200 p-4 text-xl h-16 ">
       {loading ? (
-        <>
-          <span className="loading loading-bars text-primary"></span>
-        </>
+        <span className="loading loading-dots loading-xl text-gray-700" />
+      ) : password ? (
+        <span className="text-gray-200">{password}</span>
       ) : (
-        <span>{password}</span>
+        <span className="text-gray-700">{"P4$5W0rD!"}</span>
       )}
       <img src="/icon-copy.svg" alt="" />
     </p>
